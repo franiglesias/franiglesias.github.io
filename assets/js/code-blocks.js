1f7fc5cbc82b53d9af598206d7fa656a11d07f01
Fran Iglesias <franiglesias@mac.com>
2023-06-30 17:20:14 +0200

    Button doesn't move with code block scroll

--- assets/js/code-blocks.js
+++ assets/js/code-blocks.js
@@ -1,7 +1,7 @@
 var codeBlocks = document.querySelectorAll('pre.highlight');
 
 codeBlocks.forEach(function (codeBlock) {
-    var copyButton = document.createElement('button');
+    const copyButton = document.createElement('button');
     copyButton.className = 'copy';
     copyButton.type = 'button';
     copyButton.ariaLabel = 'Copy code to clipboard';
@@ -10,11 +10,11 @@ codeBlocks.forEach(function (codeBlock) {
     codeBlock.append(copyButton);
 
     copyButton.addEventListener('click', function () {
-        var code = codeBlock.querySelector('code').innerText.trim();
+        const code = codeBlock.querySelector('code').innerText.trim();
         window.navigator.clipboard.writeText(code);
 
         copyButton.innerText = 'Copied';
-        var fourSeconds = 4000;
+        const fourSeconds = 4000;
 
         setTimeout(function () {
             copyButton.innerText = 'Copy';
